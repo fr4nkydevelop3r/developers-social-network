4c234d196c8509de76751a7fa346682fa9095be5
Frank Andrade <fr4nky.develop3r@gmail.com>
2020-04-23 01:02:24 -0400

    feat: create routes and register users to db

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const connectDB = require('./utils/db')
+const {json, urlencoded} = require('body-parser')
+
 const app = express();
 
+connectDB();
+
+//Init middleware
+//For access to the body of the request
+app.use(json())
+app.use(urlencoded({ extended: true }))
 app.get('/', (req,res) => res.send('Server running..'))
+
+//Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+
+
 
 const PORT = process.env.PORT || 5000;
 
---
+++ utils/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true);
+const config = require('config');
+
+const db = config.get('mongoURI');
+
+const connectDB  = async () => {
+    try{
+        await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log('MongoDB connected');
+
+    } catch(e){
+        console.log('Could not connect to db..')
+        console.error(e.message);
+        //Exit process with failure
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
+
